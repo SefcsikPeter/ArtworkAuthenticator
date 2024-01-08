038b8724ee1a2bbd4996b76f28ef7c3a02d707c2
Peter Sefcsik <e12025978@student.tuwien.ac.at>
2024-01-08 19:29:20 +0100

    Added artwork dto and service

---
+++ frontend/src/app/dto/artwork.ts
@@ -0,0 +1,10 @@
+import {Artist} from './artist';
+
+export interface Artwork {
+  id?: number;
+  title: string;
+  artist: Artist;
+  gallery: string;
+  price: string;
+  description?: string;
+}
