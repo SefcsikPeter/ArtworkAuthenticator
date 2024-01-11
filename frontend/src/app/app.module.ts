8d3e1c91486ec56402238c62dcdb6ba2bdb296a3
Peter Sefcsik <e12025978@student.tuwien.ac.at>
2024-01-11 13:53:23 +0100

    Removed unnecessary files

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,14 +5,10 @@ import {HttpClientModule} from '@angular/common/http';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import {ToastrModule} from 'ngx-toastr';
-
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
-import {AutocompleteComponent} from './component/autocomplete/autocomplete.component';
 import {HeaderComponent} from './component/header/header.component';
-import {HorseCreateEditComponent} from './component/horse/horse-create-edit/horse-create-edit.component';
-import {HorseComponent} from './component/horse/horse.component';
 import { HomeComponent } from './component/home/home.component';
 import { ResultPageComponent } from './component/result-page/result-page.component';
 
@@ -20,9 +16,6 @@ import { ResultPageComponent } from './component/result-page/result-page.compone
   declarations: [
     AppComponent,
     HeaderComponent,
-    HorseComponent,
-    HorseCreateEditComponent,
-    AutocompleteComponent,
     HomeComponent,
     ResultPageComponent,
   ],
