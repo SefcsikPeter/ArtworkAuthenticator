e2dcad4952a1175d98abb46042995856dae74a79
Peter Sefcsik <e12025978@student.tuwien.ac.at>
2024-02-13 17:06:05 +0100

    Added @Component

--- backend/src/main/java/artwork/authenticator/mapper/MessageMapper.java
+++ backend/src/main/java/artwork/authenticator/mapper/MessageMapper.java
@@ -4,9 +4,11 @@ import artwork.authenticator.dto.MessageListDto;
 import artwork.authenticator.entity.Message;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
 
 import java.lang.invoke.MethodHandles;
 
+@Component
 public class MessageMapper {
   private static final Logger LOG = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
   public MessageMapper(){}
