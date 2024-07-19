1c663e6700ef6c451673cf0a759809ccaf74c432
Peter Sefcsik <e12025978@student.tuwien.ac.at>
2024-07-19 11:25:56 +0200

    Added api key check (does not work correctly right now)

--- frontend/src/app/component/api-key-form/api-key-form.component.ts
+++ frontend/src/app/component/api-key-form/api-key-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {ApiKeyServiceService} from '../../service/api-key-service.service';
 
 @Component({
   selector: 'app-api-key-form',
@@ -6,10 +7,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./api-key-form.component.scss']
 })
 export class ApiKeyFormComponent implements OnInit {
+  apiKey = '';
+  validationResult: boolean | undefined;
+  errorMessage = '';
+  submitted = false;
 
-  constructor() { }
+  constructor(
+    private apiKeyService: ApiKeyServiceService
+  ) { }
 
   ngOnInit(): void {
   }
 
+  validateApiKey(): void {
+    this.submitted = true;
+    this.apiKeyService.checkApiKey(this.apiKey).subscribe(
+      isValid => {
+        this.validationResult = isValid;
+        this.errorMessage = '';
+      },
+      error => {
+        this.validationResult = false;
+        this.errorMessage = 'An error occurred while validating the API key.';
+        console.error('An error occurred:', error);
+      }
+    );
+  }
 }
--- frontend/src/app/service/api-key-service.service.ts
+++ frontend/src/app/service/api-key-service.service.ts
@@ -1,9 +1,31 @@
 import { Injectable } from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {Observable, of} from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
+
+const openaiUrl = 'https://api.openai.com/v1/models';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiKeyServiceService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  checkApiKey(apiKey: string): Observable<boolean> {
+    const headers = new HttpHeaders({
+      // eslint-disable-next-line @typescript-eslint/naming-convention
+      Authorization: `Bearer ${apiKey}`
+    });
+    return this.http.get(openaiUrl, { headers }).pipe(
+      map(() => true),
+      catchError((error) => {
+        if (error.status === 401) {
+          return of(false);
+        } else {
+          throw error;
+        }
+      })
+    );
+  }
 }
