2e12f5019320ed7e17872aaefc97eee1c133437e
Peter Sefcsik <e12025978@student.tuwien.ac.at>
2024-07-19 10:06:49 +0200

    Added api key form component

---
+++ frontend/src/app/component/api-key-form/api-key-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-api-key-form',
+  templateUrl: './api-key-form.component.html',
+  styleUrls: ['./api-key-form.component.scss']
+})
+export class ApiKeyFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
